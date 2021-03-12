fe2d36435e4b7242231f8bfa597b16fb903ddbc6
Bram De vroey <bramdevroey@gmail.com>
2021-03-12 15:40:30 +0100

    SOme features engineered

--- trees.py
+++ trees.py
@@ -28,13 +28,35 @@ if __name__ == "__main__":
             df["the_geom"].apply(lambda x: x[7:-1].split(" ")[1]).astype("float64")
         )
 
-        # drop redundant columns
-        df.drop(["the_geom", "state", "x_sp", "y_sp"], axis=1, inplace=True)
-
         # Change yes/no values to 1/0
         df.loc[:, "root_stone":"brnch_othe"] = df.loc[
             :, "root_stone":"brnch_othe"
         ].replace(to_replace=["No", "Yes"], value=[0, 1])
+
+        # consolidate option columns
+        df["curb_loc"].replace(["OnCurb", "OffsetCurb"], [1, 0], inplace=True)
+        df["sidewalk"].replace(["Damage", "NoDamage"], [1, 0], inplace=True)
+        df.rename(
+            columns={"curb_loc": "is_OnCurb", "sidewalk": "sidewalk_damaged"},
+            inplace=True,
+        )
+
+        status = pd.get_dummies(df["status"], drop_first=True)
+        df["is_dead"] = status.iloc[:, 0]
+        df["is_stump"] = status.iloc[:, 1]
+
+        df["health"].replace(["Good", "Fair", "Poor"], [2, 1, 0], inplace=True)
+
+        df["steward"].replace(
+            ["None", "1or2", "3or4", "4orMore"], [0, 1, 3, 4], inplace=True
+        )
+
+        # drop redundant columns
+        df.drop(
+            ["the_geom", "spc_latin", "state", "x_sp", "y_sp", "status"],
+            axis=1,
+            inplace=True,
+        )
 
         # write output file
         df.to_csv(argv[2])
