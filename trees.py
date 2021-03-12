2f6a9449777a8fb1e473cb832599b7538bee38f9
Bram De vroey <bramdevroey@gmail.com>
2021-03-12 11:08:45 +0100

    trees position replaced with geom

--- trees.py
+++ trees.py
@@ -16,8 +16,18 @@ if __name__ == "__main__":
             argv[1], parse_dates=[[0, 1]], dtype={"zip_code": pd.UInt16Dtype()}
         )
 
-        # import
+        # import data
         df = pd.read_csv(argv[1], parse_dates=[0])
+        df.set_index("tree_id", inplace=True)
+
+        # replace long and lat with data from the_geom
+        df["longitude"] = (
+            df["the_geom"].apply(lambda x: x[7:-1].split(" ")[0]).astype("float64")
+        )
+        df["latitude"] = (
+            df["the_geom"].apply(lambda x: x[7:-1].split(" ")[1]).astype("float64")
+        )
+        df.drop("the_geom", axis=1, inplace=True)
 
         # write output file
         df.to_csv(argv[2])
